297c9e35a7e1865016e1f08b4bb2c6350ef5f6d9
Liam Brand <liamtbrand@gmail.com>
2019-08-10 23:01:37 +1200

    Fix install script

--- install.sh
+++ install.sh
@@ -3,23 +3,28 @@
 
 # Add a user and group for the ipnotify user
 echo "[IPNotify]: Creating user and group."
-sudo adduser --system --group ipnotify
+sudo adduser --system --no-create-home --group ipnotify
 
 # Copy the ipnotify app into /opt/ipnotify
 echo "[IPNotify]: Copying files to /opt/ipnotify."
-su ipnotify -c 'mkdir /opt/ipnotify/'
-su ipnotify -c 'cp -r src/ /opt/ipnotify/'
-su ipnotify -c 'cp README.md /opt/ipnotify/'
+sudo mkdir /opt/ipnotify/
+sudo chown ipnotify /opt/ipnotify/
+sudo chgrp ipnotify /opt/ipnotify/
+sudo -u ipnotify cp -r src/* /opt/ipnotify/
+sudo -u ipnotify cp README.md /opt/ipnotify/
 
 # Create configuration files
 echo "[IPNotify]: Creating configuration files..."
-su ipnotify -c 'mkdir /etc/opt/ipnotify/'
-su ipnotify -c 'cp config/smtp.conf /etc/opt/ipnotify/smtp.conf'
-su ipnotify -c 'cp config/mailinglist /etc/opt/ipnotify/mailinglist'
+sudo mkdir /etc/opt/ipnotify/
+sudo chown ipnotify /etc/opt/ipnotify/
+sudo chgrp ipnotify /etc/opt/ipnotify/
+sudo -u ipnotify cp -r config/* /etc/opt/ipnotify/
 
 # Create ip address file
-su ipnotify -c 'mkdir /var/opt/ipnotify/'
-su ipnotify -c 'touch /var/opt/ipnotify/ip_address'
+sudo mkdir /var/opt/ipnotify/
+sudo chown ipnotify /var/opt/ipnotify/
+sudo chgrp ipnotify /var/opt/ipnotify/
+sudo -u ipnotify touch /var/opt/ipnotify/ip_address
 
 # Install the service into /etc/systemd/system
 sudo cp service/ipnotify.service /etc/systemd/system/ipnotify.service
