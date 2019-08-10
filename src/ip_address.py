02b8d9c4857dd8c22cf9c42502736e4397b7ab0a
Liam Brand <liamtbrand@gmail.com>
2019-08-10 22:06:04 +1200

    Add ip address manager

---
+++ src/ip_address.py
@@ -0,0 +1,21 @@
+ip_address_file = '/var/opt/ipnotify/ip_address'
+
+def read():
+	''' Reads the ip address from file.
+	:returns: The ip address in the file.
+	'''
+	with open(ip_address_file,'r') as f:
+		return f.read()
+
+def write(ip_address):
+	''' Writes the ip address to file.
+	:param ip_address: The ip address to write.
+	'''
+	with open(ip_address_file,'w') as f:
+
+fetch_url = 'http://ip.42.pl/raw-master-server-ip'
+
+def fetch():
+	''' Fetches the current ip address from the web.
+	'''
+    return str(urlopen(fetch_url).read())[2:-1]
